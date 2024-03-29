6d7653679b7aaebaee221c209ea2d152a87e8a16
Guo Xianglong <2955839148@qq.com>
2024-03-29 16:22:28 +0800

    fix

---
+++ Readme.md
@@ -0,0 +1,130 @@
+## 操作指南
+跟着提示输入即可
+
+#### 0
+输入使用哪个参数运算，E or Q
+#### 1
+输入材料性质
+#### 2
+输入是每层一样厚还是不一样，输入厚度和角度
+#### 3 
+输入偏转角
+#### 4
+得到结果
+
+
+## 测试
+#### 1
+使用E，Q计算 E:1,Q:2
+1
+imput the value of E1
+1
+imput the value of E2
+1
+imput the value of nu12
+0
+imput the value of nu21
+0
+imput the value of G12
+1
+imput 1 for same layer, 2 for different layer 输入1是相同层厚，2是不同层厚
+2
+imput the number of layers
+4
+imput the thickness of layer 1
+1
+imput the angle of layer 1
+0
+imput the thickness of layer 2
+1
+imput the angle of layer 2
+90
+imput the thickness of layer 3
+1
+imput the angle of layer 3
+45
+imput the thickness of layer 4
+1
+imput the angle of layer 4
+-45
+输入偏转角度
+20
+[[ 5. -1.  0.]
+ [-1.  5. -0.]
+ [ 0. -0.  3.]]
+[[ 0.1736 -0.1736  0.9848]
+ [-0.1736  0.1736 -0.9848]
+ [ 0.9848 -0.9848 -0.1736]]
+[[ 6.6667 -1.3333  0.    ]
+ [-1.3333  6.6667  0.    ]
+ [ 0.     -0.      4.    ]]
+#### 2
+使用E，Q计算 E:1,Q:2
+1
+imput the value of E1
+1
+imput the value of E2
+1
+imput the value of nu12
+0
+imput the value of nu21
+0
+imput the value of G12
+1
+imput 1 for same layer, 2 for different layer 输入1是相同层厚，2是不同层厚
+1
+imput the number of layers
+4
+imput the thickness of layer
+1
+imput the angle of layer 1
+0
+imput the angle of layer 2
+90
+imput the angle of layer 3
+45
+imput the angle of layer 4
+-45
+输入偏转角度
+20
+[[ 5. -1.  0.]
+ [-1.  5. -0.]
+ [ 0. -0.  3.]]
+[[ 0.1736 -0.1736  0.9848]
+ [-0.1736  0.1736 -0.9848]
+ [ 0.9848 -0.9848 -0.1736]]
+[[ 6.6667 -1.3333  0.    ]
+ [-1.3333  6.6667  0.    ]
+ [ 0.     -0.      4.    ]]
+结果相同
+#### 3
+使用E，Q计算 E:1,Q:2
+2
+imput the value of Q11
+1
+imput the value of Q12
+1
+imput the value of Q22
+1
+imput the value of Q66
+1
+imput 1 for same layer, 2 for different layer 输入1是相同层厚，2是不同层厚
+1
+imput the number of layers
+1
+imput the thickness of layer
+1
+imput the angle of layer 1
+1
+输入偏转角度
+1
+[[ 1.0036  0.9951 -0.0696]
+ [-0.0024  1.0036  0.0348]
+ [-0.0348  0.0696  0.9964]]
+[[0. 0. 0.]
+ [0. 0. 0.]
+ [0. 0. 0.]]
+[[ 0.0836  0.0829 -0.0058]
+ [-0.0002  0.0836  0.0029]
+ [-0.0029  0.0058  0.083 ]]
+ 成功输出
--- main.py
+++ main.py
@@ -62,12 +62,41 @@ def Q_matrix(E1,E2,nu12,nu21,G12,theta):
     T[2,2] = math.cos(theta)**2-math.sin(theta)**2
     Q_bar = numpy.dot(numpy.dot(numpy.linalg.inv(T),Q),numpy.linalg.inv(T.T))
     return Q_bar
+def Q_matrix_1():
+    Q = numpy.zeros((3,3))
+    #theta = math.radians(theta)
+    print('imput the value of Q11')
+    Q[0,0] = float(input())
+    print('imput the value of Q12')
+    Q[0,1] = float(input())
+    print('imput the value of Q22')
+    Q[1,1] = float(input())
+    print('imput the value of Q66')
+    Q[2,2] = float(input())
+    return Q
+def Q_matrix_2(Q,theta):
+    T = numpy.zeros((3,3))
+    T[0,0] = math.cos(theta)**2
+    T[0,1] = math.sin(theta)**2
+    T[0,2] = 2*math.cos(theta)*math.sin(theta)
+    T[1,0] = math.sin(theta)**2
+    T[1,1] = math.cos(theta)**2
+    T[1,2] = -2*math.cos(theta)*math.sin(theta)
+    T[2,0] = -math.cos(theta)*math.sin(theta)
+    T[2,1] = math.cos(theta)*math.sin(theta)
+    T[2,2] = math.cos(theta)**2-math.sin(theta)**2
+    Q_bar = numpy.dot(numpy.dot(numpy.linalg.inv(T),Q),numpy.linalg.inv(T.T))
+    return Q_bar
 #计算转变A矩阵
-def A_matrix(E1,E2,nu12,nu21,G12,h,theta):
+def A_matrix(E1,E2,nu12,nu21,G12,h,theta,Qmode=1):
     list = []
     for i in theta:
         i=math.radians(i)
-        list.append(Q_matrix(E1,E2,nu12,nu21,G12,i))
+        if Qmode.__class__==int:
+        
+            list.append(Q_matrix(E1,E2,nu12,nu21,G12,i))
+        else:
+            list.append(Q_matrix_2(Qmode,i))
     A = numpy.zeros((3,3))
     if h.__class__ == float:
         for i in list:
@@ -77,11 +106,15 @@ def A_matrix(E1,E2,nu12,nu21,G12,h,theta):
             A = A + list[i]*h[i]
     return A
 #计算B矩阵
-def B_matrix(E1,E2,nu12,nu21,G12,h,theta):
+def B_matrix(E1,E2,nu12,nu21,G12,h,theta,Qmode=1):
     list = []
     for i in theta:
         i=math.radians(i)
-        list.append(Q_matrix(E1,E2,nu12,nu21,G12,i))
+        if Qmode.__class__==int:
+        
+            list.append(Q_matrix(E1,E2,nu12,nu21,G12,i))
+        else:
+            list.append(Q_matrix_2(Qmode,i))
     B = numpy.zeros((3,3))
     if h.__class__ == float:
         t=h*len(theta)
@@ -92,21 +125,26 @@ def B_matrix(E1,E2,nu12,nu21,G12,h,theta):
     else:
         t=0
         for i in h:
-            t==t+i
-        for i in range(len(h)):
+            t=t+i
+        for k in range(len(h)):
             z0=-t/2
-            for i in range(i):
+            z1=-t/2
+            for i in range(k):
                 z0=z0+h[i]
-            for i in range(i+1):
-                z1=z0+h[i]
-            B = B + (z1**2-z0**2)/2*(list[i])
+            for i in range(k+1):
+                z1=z1+h[i]
+            B = B + (z1**2-z0**2)/2*(list[k])
     return B
 #计算D矩阵
-def D_matrix(E1,E2,nu12,nu21,G12,h,theta):
+def D_matrix(E1,E2,nu12,nu21,G12,h,theta,Qmode=1):
     list = []
     for i in theta:
         i=math.radians(i)
-        list.append(Q_matrix(E1,E2,nu12,nu21,G12,i))
+        if Qmode.__class__==int:
+        
+            list.append(Q_matrix(E1,E2,nu12,nu21,G12,i))
+        else:
+            list.append(Q_matrix_2(Qmode,i))
     D = numpy.zeros((3,3))
     if h.__class__ == float:
         t=h*len(theta)
@@ -117,36 +155,66 @@ def D_matrix(E1,E2,nu12,nu21,G12,h,theta):
     else:
         t=0
         for i in h:
-            t==t+i
-        for i in range(len(h)):
+            t=t+i
+        for k in range(len(h)):
             z0=-t/2
-            for i in range(i):
+            z1=-t/2
+            for i in range(k):
                 z0=z0+h[i]
-            for i in range(i+1):
-                z1=z0+h[i]
-            D = D + (z1**3-z0**3)/3*(list[i])
+            for i in range(k+1):
+                z1=z1+h[i]
+            D = D + (z1**3-z0**3)/3*(list[k])
     return D
 
 if __name__ == '__main__':
-    E1,E2,nu12,nu21,G12 = Get_value()
-    print('imput 1 for same layer, 2 for different layer',"输入1是相同层厚，2是不同层厚")
+    print('使用E，Q计算','E:1,Q:2')
     choice = int(input())
     if choice == 1:
-        h,theta = Get_same_layer()
+        E1,E2,nu12,nu21,G12 = Get_value()
+        print('imput 1 for same layer, 2 for different layer',"输入1是相同层厚，2是不同层厚")
+        choice = int(input())
+        if choice == 1:
+            h,theta = Get_same_layer()
+        else:
+            h,theta = Get_dif_layer()
+        print('输入偏转角度')
+        alpha = float(input())
+        theta = [i+alpha for i in theta]
+        A = A_matrix(E1,E2,nu12,nu21,G12,h,theta)
+        numpy.around(A,decimals=4,out=A)
+        print(A)
+        B=B_matrix(E1,E2,nu12,nu21,G12,h,theta)
+        numpy.around(B,decimals=4,out=B)
+        print(B)
+        D=D_matrix(E1,E2,nu12,nu21,G12,h,theta)
+        numpy.around(D,decimals=4,out=D)
+        print(D)
     else:
-        h,theta = Get_dif_layer()
-    print('输入偏转角度')
-    alpha = float(input())
-    theta = [i+alpha for i in theta]
-    A = A_matrix(E1,E2,nu12,nu21,G12,h,theta)
-    numpy.around(A,decimals=4,out=A)
-    print(A)
-    B=B_matrix(E1,E2,nu12,nu21,G12,h,theta)
-    numpy.around(B,decimals=4,out=B)
-    print(B)
-    D=D_matrix(E1,E2,nu12,nu21,G12,h,theta)
-    numpy.around(D,decimals=4,out=D)
-    print(D)
-
-
-
+        Q=Q_matrix_1()
+        print('imput 1 for same layer, 2 for different layer',"输入1是相同层厚，2是不同层厚")
+        choice = int(input())
+        if choice == 1:
+            h,theta = Get_same_layer()
+        else:
+            h,theta = Get_dif_layer()
+        print('输入偏转角度')
+        alpha = float(input())
+        theta = [i+alpha for i in theta]
+        A = A_matrix(0,0,0,0,0,h,theta,Q)
+        numpy.around(A,decimals=4,out=A)
+        print(A)
+        B=B_matrix(0,0,0,0,0,h,theta,Q)
+        numpy.around(B,decimals=4,out=B)
+        print(B)
+        D=D_matrix(0,0,0,0,0,h,theta,Q)
+        numpy.around(D,decimals=4,out=D)
+        print(D)
+"""[[ 5. -1.  0.]
+ [-1.  5. -0.]
+ [ 0. -0.  3.]]
+[[ 0.1736 -0.1736  0.9848]
+ [-0.1736  0.1736 -0.9848]
+ [ 0.9848 -0.9848 -0.1736]]
+[[ 6.6667 -1.3333  0.    ]
+ [-1.3333  6.6667  0.    ]
+ [ 0.     -0.      4.    ]]"""
